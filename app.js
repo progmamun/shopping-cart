711f4f68029a7c21516ca63d2e0f94c1ba1c78aa
progmamun <mamun333089@gmail.com>
2022-02-12 23:20:25 +0600

    case-button-update

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('I am in a separate file')
+document.getElementById('case-plus').addEventListener('click', function () {
+  const caseInput = document.getElementById('case-number');
+  const caseNumber = caseInput.value;
+  caseInput.value = parseInt(caseNumber) + 1;
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+  const caseInput = document.getElementById('case-number');
+  const caseNumber = caseInput.value;
+
+  caseInput.value = parseInt(caseNumber) - 1;
+});
