efad640c979aed66e118a9ba8ce4006674310990
progmamun <mamun333089@gmail.com>
2022-02-13 12:24:11 +0600

    update

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-function updateCaseNumber(isIncreasing) {
-  const caseInput = document.getElementById('case-number');
-  let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+  const productInput = document.getElementById(product + '-number');
+  let productNumber = productInput.value;
   if (isIncreasing == true) {
-    caseInput.value = parseInt(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-    caseInput.value = parseInt(caseNumber) - 1;
+    productInput.value = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productInput.value = parseInt(productNumber) - 1;
   }
-  caseInput.value = caseNumber;
+  productInput.value = productNumber;
   // update case total
-  const caseTotal = document.getElementById('case-total');
-  caseTotal.innerText = caseNumber * 59;
+  const productTotal = document.getElementById(product + '-total');
+  productTotal.innerText = productNumber * price;
 }
-
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+  updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+  updateProductNumber('phone', 1219, false);
+});
+// handle case increase decrease event
 document.getElementById('case-plus').addEventListener('click', function () {
-  const caseInput = document.getElementById('case-number');
-  updateCaseNumber(true);
+  // const caseInput = document.getElementById('case-number');
+  updateProductNumber('case', 59, true);
   // const caseNumber = caseInput.value;
   // caseInput.value = parseInt(caseNumber) + 1;
 });
 document.getElementById('case-minus').addEventListener('click', function () {
-  const caseInput = document.getElementById('case-number');
-  updateCaseNumber(false);
+  // const caseInput = document.getElementById('case-number');
+  updateProductNumber('case', 59, false);
   // const caseNumber = caseInput.value;
   // caseInput.value = parseInt(caseNumber) - 1;
 });
