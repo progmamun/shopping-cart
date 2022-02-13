ed02bd8766d47a6e175c5476cb150fdfe15d0e52
progmamun <mamun333089@gmail.com>
2022-02-13 13:25:39 +0600

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function updateProductNumber(product, price, isIncreasing) {
     productInput.value = parseInt(productNumber) - 1;
   }
   productInput.value = productNumber;
-  // update case total
+  // update total
   const productTotal = document.getElementById(product + '-total');
   productTotal.innerText = productNumber * price;
 }
