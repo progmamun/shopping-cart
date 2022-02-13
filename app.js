bfdc8bd5fb430bf5223c34b30aca27190aea4f95
progmamun <mamun333089@gmail.com>
2022-02-13 14:45:17 +0600

    fix-bug

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ function updateProductNumber(product, price, isIncreasing) {
   const productInput = document.getElementById(product + '-number');
   let productNumber = productInput.value;
   if (isIncreasing == true) {
-    productInput.value = parseInt(productNumber) + 1;
+    productNumber = parseInt(productNumber) + 1;
   } else if (productNumber > 0) {
-    productInput.value = parseInt(productNumber) - 1;
+    productNumber = parseInt(productNumber) - 1;
   }
   productInput.value = productNumber;
   // update total
   const productTotal = document.getElementById(product + '-total');
   productTotal.innerText = productNumber * price;
+
+  // calculate total
+  calculateTotal();
+}
+
+function getInputValue(product) {
+  const productInput = document.getElementById(product + '-number');
+  const productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+
+function calculateTotal() {
+  const phoneTotal = getInputValue('phone') * 1219;
+  const caseTotal = getInputValue('case') * 59;
+  const subTotal = phoneTotal + caseTotal;
+  // update on the html
+  document.getElementById('sub-total').innerText = subTotal;
 }
 // phone increase decrease events
 document.getElementById('phone-plus').addEventListener('click', function () {
