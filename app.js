cc2ec2c2759fbb14c0caa2cc0ee7f413601a57c8
progmamun <mamun333089@gmail.com>
2022-02-13 11:21:07 +0600

    case-button-update

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+function updateCaseNumber(isIncreasing) {
   const caseInput = document.getElementById('case-number');
   const caseNumber = caseInput.value;
-  caseInput.value = parseInt(caseNumber) + 1;
+  if (isIncreasing == true) {
+    caseInput.value = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseInput.value = parseInt(caseNumber) - 1;
+  }
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+  const caseInput = document.getElementById('case-number');
+  updateCaseNumber(true);
+  // const caseNumber = caseInput.value;
+  // caseInput.value = parseInt(caseNumber) + 1;
 });
 document.getElementById('case-minus').addEventListener('click', function () {
   const caseInput = document.getElementById('case-number');
-  const caseNumber = caseInput.value;
-
-  caseInput.value = parseInt(caseNumber) - 1;
+  updateCaseNumber(false);
+  // const caseNumber = caseInput.value;
+  // caseInput.value = parseInt(caseNumber) - 1;
 });
